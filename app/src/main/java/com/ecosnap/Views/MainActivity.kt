08491b1f46cdc6acb5e4843c95d3e76770a87be2
aaronluannguyen <aaronluannguyen@gmail.com>
2018-05-24 23:35:22 -0700

    Added main home activity

---
+++ app/src/main/java/com/ecosnap/Views/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ecosnap.Views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ecosnap.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

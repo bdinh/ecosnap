0908920f7cd6460e5112962afc99d356d022f818
aaronluannguyen <aaronluannguyen@gmail.com>
2018-05-31 04:44:38 -0700

    Finished implementing needed functions for nearby places. Running into java to kotlin code error

--- app/src/main/java/com/ecosnap/Controller/MapController.kt
+++ app/src/main/java/com/ecosnap/Controller/MapController.kt
@@ -2,11 +2,26 @@ package com.ecosnap.Controller
 
 import android.app.Activity
 import android.content.Context
+import android.os.AsyncTask
+import android.util.Log
+import android.widget.Toast
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 fun CheckGooglePlayServices(ctx: Context, activity: Activity): Boolean {
     val googleAPI = GoogleApiAvailability.getInstance()
@@ -30,12 +45,134 @@ fun buildGoogleApiClient(ctx: Context, connCB: GoogleApiClient.ConnectionCallbac
     mGoogleApiClient.connect()
 }
 
-//fun getNearbyRecycleCneters(map: GoogleMap) {
-//    map.clear()
-//    val recycleCenters = "Recycle Centers"
-//    val url: String = getUrl(latitude, longitude, recycleCenters)
-//    val DataTransfer = []
-//    DataTransfer[0] = map
-//    DataTransfer[1] = url
-//    val nearbyRecycleCenters = GetNearByPlacesData()
-//}
+class DataParser {
+    fun parse(jsonData: String): List<HashMap<String, String>> {
+        lateinit var jsonArray: JSONArray
+        lateinit var jsonObj: JSONObject
+
+        try {
+            jsonObj = JSONObject(jsonData)
+            jsonArray = jsonObj.getJSONArray("results")
+            return getPlaces(jsonArray)
+        } catch (e: JSONException) {
+            Log.i("ECOSNAP.ERROR", e.toString())
+            return emptyList()
+        }
+    }
+
+    private fun getPlaces(jsonArray: JSONArray): List<HashMap<String, String>> {
+        var placesCount = jsonArray.length()
+        var placesList = ArrayList<HashMap<String, String>>()
+        lateinit var placeMap: HashMap<String, String>
+
+        for (i in 0..(placesCount - 1)) {
+            try {
+                placeMap = getPlace(jsonArray.get(i) as JSONObject)
+                placesList.add(placeMap)
+            } catch (e: JSONException) {
+                Log.i("ECOSNAP.ERROR", e.toString())
+            }
+        }
+        return placesList
+    }
+
+    private fun getPlace(googlePlaceJson: JSONObject): HashMap<String, String> {
+        var googlePlaceMap = HashMap<String, String>()
+        try {
+            var placeName = "-NA-"
+            if (!googlePlaceJson.isNull("name")) {
+                placeName = googlePlaceJson.getString("name")
+            }
+            var vicinity = "-NA-"
+            if (!googlePlaceJson.isNull("vicinity")) {
+                vicinity = googlePlaceJson.getString("vicinity")
+            }
+            var latitude = googlePlaceJson.getJSONObject("geometry").getJSONObject("location").getString("lat")
+            var longitude = googlePlaceJson.getJSONObject("geometry").getJSONObject("location").getString("lng")
+            var reference = googlePlaceJson.getString("reference")
+            googlePlaceMap.put("place_name", placeName)
+            googlePlaceMap.put("vicinity", vicinity)
+            googlePlaceMap.put("lat", latitude)
+            googlePlaceMap.put("lng", longitude)
+            googlePlaceMap.put("reference", reference)
+            return googlePlaceMap
+        } catch (e: JSONException) {
+            Log.i("ECOSNAP.ERROR", e.toString())
+            return googlePlaceMap
+        }
+    }
+}
+
+class GetNearbyRecycleCenters: AsyncTask<Any, String, String>() {
+    private lateinit var googlePlaceData: String
+    private lateinit var map: GoogleMap
+    private lateinit var url: String
+
+    override fun doInBackground(vararg objects: Any): String {
+        map = objects[0] as GoogleMap
+        url = objects[1] as String
+        try {
+            val downloadUrl = DownloadUrl()
+            googlePlaceData = downloadUrl.readUrl(url)
+            return googlePlaceData
+        } catch (e: Exception) {
+            Log.i("ECOSNAP.ERROR Line 120", e.toString())
+            return ""
+        }
+    }
+
+    override fun onPostExecute(result: String) {
+        lateinit var nearbyPlacesList: List<HashMap<String, String>>
+        var dataParser = DataParser()
+        nearbyPlacesList = dataParser.parse(result)
+        showNearbyPlaces(nearbyPlacesList)
+    }
+
+    private fun showNearbyPlaces(nearbyPlacesList: List<HashMap<String, String>>) {
+        for (i in nearbyPlacesList.indices) {
+            var markerOptions = MarkerOptions()
+            var googlePlace: HashMap<String, String> = nearbyPlacesList.get(i)
+            var lat = googlePlace.get("lat")?.toDouble()
+            var lng = googlePlace.get("lng")?.toDouble()
+            var placeName = googlePlace.get("place_name")
+            var vicinity = googlePlace.get("vicinity")
+            var latLng = LatLng(lat as Double, lng as Double)
+            markerOptions.position(latLng)
+            markerOptions.title(placeName + " : " + vicinity)
+            map.addMarker(markerOptions)
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+            map.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+            map.animateCamera(CameraUpdateFactory.zoomTo(11f))
+        }
+    }
+}
+
+class DownloadUrl {
+    fun readUrl(strUrl: String): String {
+        var data = ""
+        lateinit var inputStream: InputStream
+        lateinit var urlConnection: HttpURLConnection
+
+        try {
+            val url = URL(strUrl)
+            urlConnection = url.openConnection() as HttpURLConnection
+            urlConnection.connect()
+            inputStream = urlConnection.inputStream
+            val br = BufferedReader(InputStreamReader(inputStream))
+            val sb = StringBuffer()
+            var line = br.readLine()
+            while (line != null) {
+                sb.append(line)
+                line = br.readLine()
+            }
+            data = sb.toString()
+            br.close()
+        } catch (e: Exception) {
+            Log.i("ECOSNAP.ERROR", e.toString())
+        } finally {
+            inputStream.close()
+            urlConnection.disconnect()
+            return data
+        }
+    }
+}
--- app/src/main/java/com/ecosnap/Views/MapsActivity.kt
+++ app/src/main/java/com/ecosnap/Views/MapsActivity.kt
@@ -1,12 +1,18 @@
 package com.ecosnap.Views
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.ecosnap.Controller.GetNearbyRecycleCenters
 import com.ecosnap.R
+import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,6 +25,8 @@ import com.google.android.gms.maps.model.Marker
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val REQUEST_CHECK_SETTINGS = 2
+        private const val PROXIMITY_RADIUS = 100000
     }
 
     private lateinit var map: GoogleMap
@@ -27,6 +35,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     private lateinit var locationCallBack: LocationCallback
     private lateinit var locationRequest: LocationRequest
     private var locationUpdateState = false
+    private lateinit var currLocation: LatLng
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +47,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         mapFragment.getMapAsync(this)
 
         fCL = LocationServices.getFusedLocationProviderClient(this)
+        locationCallBack = object : LocationCallback() {}
+        createLocationRequest()
     }
 
 
@@ -59,9 +70,33 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         map.setOnMarkerClickListener(this)
 
         setUpMap()
+
+
     }
 
     override fun onMarkerClick(p0: Marker?) = false
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                locationUpdateState = true
+                startLocationUpdates()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fCL.removeLocationUpdates(locationCallBack)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!locationUpdateState) {
+            startLocationUpdates()
+        }
+    }
 
     private fun setUpMap() {
         if (ActivityCompat.checkSelfPermission(this,
@@ -75,9 +110,62 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         fCL.lastLocation.addOnSuccessListener(this) {location ->
             if (location != null) {
                 lastLocation = location
-                val currentLatLng = LatLng(location.latitude, location.longitude)
+                var currentLatLng = LatLng(location.latitude, location.longitude)
+                currLocation = currentLatLng
+                Toast.makeText(this, currLocation.toString(), Toast.LENGTH_LONG).show()
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                var url = getUrl(currLocation.latitude, currLocation.longitude, "recycle center")
+                var dataTransfer = Array<Any>(2, {})
+                dataTransfer[0] = map
+                dataTransfer[1] = url
+                var getNearbyRCenters = GetNearbyRecycleCenters()
+                getNearbyRCenters.execute(dataTransfer)
             }
         }
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        fCL.requestLocationUpdates(locationRequest, locationCallBack, null)
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.interval = 10000
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener {
+            locationUpdateState = true
+            startLocationUpdates()
+        }
+        task.addOnFailureListener {e ->
+            if (e is ResolvableApiException) {
+                try {
+                    e.startResolutionForResult(this@MapsActivity, REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    //Ignore the error
+                }
+            }
+        }
+    }
+
+    private fun getUrl(lat: Double, lng: Double, nearbyPlace: String): String {
+        var googlePlacesUrl = StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?")
+        googlePlacesUrl.append("location=" + lat + "," + lng)
+        googlePlacesUrl.append("&radius=" + PROXIMITY_RADIUS)
+        googlePlacesUrl.append("&type=" + nearbyPlace)
+        googlePlacesUrl.append("&sensor=true")
+        googlePlacesUrl.append("&key=" + "AIzaSyBLgt9kCOa6jnHzUVhZuVU6x4J3COAt80Q")
+        return (googlePlacesUrl.toString())
     }
 }

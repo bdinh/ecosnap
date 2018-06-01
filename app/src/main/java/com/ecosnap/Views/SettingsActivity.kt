1529ed3dcb12af0dc05b085d533f00b9b00a1aaf
EJacobson96 <eric.jj96@gmail.com>
2018-05-31 20:22:15 -0700

    begin settings implementation

---
+++ app/src/main/java/com/ecosnap/Views/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.ecosnap.Views
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class SettingsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
